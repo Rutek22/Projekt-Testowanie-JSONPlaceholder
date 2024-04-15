a27cba2020833a930734c030e3b075eb97e8c5ad
Rutek22 <krystian.rutkowski17@gmail.com>
2024-04-15 21:16:17 +0200

    Poprawki kodu dla testow

--- test.py
+++ test.py
@@ -10,23 +10,19 @@ class TestApp(unittest.TestCase):
         app.config['TESTING'] = True
         self.app = app.test_client()
 
-
     def test_app_exists(self):
         self.assertIsNotNone(app, "Application instance does not exist")
 
-
     def test_app_instance(self):
-        self.assertIsInstance(app, Flask, "Application is not an instance of Flask")
-
+        self.assertIsInstance(app, Flask,
+                              "Application is not an instance of Flask")
 
     def test_api_url_exists(self):
         self.assertIsNotNone(API_URL, "API URL is not defined")
 
-
     def test_api_url_valid(self):
         expected_url = 'https://jsonplaceholder.typicode.com/'
         self.assertEqual(API_URL, expected_url, "API URL is not correct")
-
 
     def test_index(self):
         with app.test_request_context('/'):
@@ -34,19 +30,23 @@ class TestApp(unittest.TestCase):
             self.assertEqual(response.status_code, 200)
             self.assertIn(b'<h3>JsonPlaceholder</h3>', response.data)
 
-
     @patch('requests.get')
     def test_getAlbums(self, mocked_get):
         # Symuluj odpowiedź zapytania o albumy
         mocked_albums_response = MagicMock()
-        mocked_albums_response.json.return_value = [{'id': 1, 'title': 'Album 1'},
-                                                    {'id': 2, 'title': 'Album 2'}]
+        mocked_albums_response.json.return_value = [{'id': 1,
+                                                     'title': 'Album 1'},
+                                                    {'id': 2,
+                                                     'title': 'Album 2'}]
         # Symuluj odpowiedź zapytania o zdjęcia
         mocked_photos_response = MagicMock()
-        mocked_photos_response.json.return_value = [{'albumId': 1, 'title': 'Photo 1'},
-                                                    {'albumId': 2, 'title': 'Photo 2'}]
+        mocked_photos_response.json.return_value = [{'albumId': 1,
+                                                     'title': 'Photo 1'},
+                                                    {'albumId': 2,
+                                                     'title': 'Photo 2'}]
         # Ustaw efekt dla każdego wywołania z requests.get
-        mocked_get.side_effect = [mocked_albums_response, mocked_photos_response]
+        mocked_get.side_effect = [mocked_albums_response,
+                                  mocked_photos_response]
 
         # Wywołaj funkcję getAlbums()
         with app.test_request_context('/albums'):
@@ -58,12 +58,15 @@ class TestApp(unittest.TestCase):
             self.assertIn(b'Photo 1', response.data)
             self.assertIn(b'Photo 2', response.data)
 
-
     def test_getAlbumsWithLimit(self):
         with patch('main.requests.get') as mocked_get:
             mocked_albums_response = MagicMock()
-            mocked_albums_response.json.return_value = [{'id': 1, 'title': 'Album 1'},
-                                                        {'id': 2, 'title': 'Album 2'}]
+            mocked_albums_response.json.return_value = [{'id': 1,
+                                                         'title':
+                                                             'Album 1'},
+                                                        {'id': 2,
+                                                         'title':
+                                                             'Album 2'}]
             mocked_get.return_value = mocked_albums_response
 
             response = self.app.get('/albums/2')
@@ -71,7 +74,6 @@ class TestApp(unittest.TestCase):
             self.assertEqual(response.status_code, 200)
             self.assertIn(b'Album 1', response.data)
             self.assertIn(b'Album 2', response.data)
-
 
     @patch('requests.get')
     def test_getPhotos(self, mocked_get):
@@ -94,7 +96,6 @@ class TestApp(unittest.TestCase):
             self.assertIn(b'Photo 1', response.data)
             self.assertIn(b'Photo 2', response.data)
 
-
     @patch('requests.get')
     def test_getPhotosWithLimit(self, mocked_get):
         mocked_photos_response = MagicMock()
@@ -115,25 +116,37 @@ class TestApp(unittest.TestCase):
             self.assertIn(b'Photo 1', response.data)
             self.assertIn(b'Photo 2', response.data)
 
-
     def test_getPosts(self):
         with patch('main.requests.get') as mocked_get:
             # Symuluj odpowiedź zapytania o posty
             mocked_posts_response = MagicMock()
-            mocked_posts_response.json.return_value = [{'id': 1, 'title': 'Post 1',
-                                                        'body': 'Body of post 1'},
-                                                       {'id': 2, 'title': 'Post 2',
-                                                        'body': 'Body of post 2'}]
+            mocked_posts_response.json.return_value = [{'id': 1,
+                                                        'title':
+                                                            'Post 1',
+                                                        'body':
+                                                            'Body post 1'},
+                                                       {'id': 2,
+                                                        'title':
+                                                            'Post 2',
+                                                        'body':
+                                                            'Body post 2'}]
             # Symuluj odpowiedź zapytania o komentarze
             mocked_comments_response = MagicMock()
-            mocked_comments_response.json.return_value = [{'postId': 1, 'id': 1,
-                                                           'body': 'Comment 1'},
-                                                          {'postId': 1, 'id': 2,
-                                                           'body': 'Comment 2'},
-                                                          {'postId': 2, 'id': 3,
-                                                           'body': 'Comment 3'}]
+            mocked_comments_response.json.return_value = [{'postId': 1,
+                                                           'id': 1,
+                                                           'body':
+                                                               'Comment 1'},
+                                                          {'postId': 1,
+                                                           'id': 2,
+                                                           'body':
+                                                               'Comment 2'},
+                                                          {'postId': 2,
+                                                           'id': 3,
+                                                           'body':
+                                                               'Comment 3'}]
             # Ustaw efekt dla każdego wywołania z requests.get
-            mocked_get.side_effect = [mocked_posts_response, mocked_comments_response]
+            mocked_get.side_effect = [mocked_posts_response,
+                                      mocked_comments_response]
 
             # Wywołaj funkcję getPosts()
             with app.test_request_context('/posts'):
@@ -148,33 +161,46 @@ class TestApp(unittest.TestCase):
                 self.assertIn(b'Body of post 1', response.data)
                 self.assertIn(b'Body of post 2', response.data)
 
-                # Sprawdź, czy dane o komentarzach są obecne w treści odpowiedzi
+                # Sprawdź, czy dane o komentarzach są obecne w treści
                 self.assertIn(b'Comment 1', response.data)
                 self.assertIn(b'Comment 2', response.data)
                 self.assertIn(b'Comment 3', response.data)
-
 
     def test_getPostsWithLimit(self):
         with patch('main.requests.get') as mocked_get:
             # Symuluj odpowiedź zapytania o posty
             mocked_posts_response = MagicMock()
-            mocked_posts_response.json.return_value = [{'id': 1, 'title': 'Post 1',
-                                                        'body': 'Body of post 1'},
-                                                       {'id': 2, 'title': 'Post 2',
-                                                        'body': 'Body of post 2'}]
+            mocked_posts_response.json.return_value = [{'id': 1,
+                                                        'title':
+                                                            'Post 1',
+                                                        'body':
+                                                            'Body post 1'},
+                                                       {'id': 2,
+                                                        'title':
+                                                            'Post 2',
+                                                        'body':
+                                                            'Body post 2'}]
             # Symuluj odpowiedź zapytania o komentarze
             mocked_comments_response = MagicMock()
-            mocked_comments_response.json.return_value = [{'postId': 1, 'id': 1,
-                                                           'body': 'Comment 1'},
-                                                          {'postId': 1, 'id': 2,
-                                                           'body': 'Comment 2'},
-                                                          {'postId': 2, 'id': 3,
-                                                           'body': 'Comment 3'}]
+            mocked_comments_response.json.return_value = [{'postId': 1,
+                                                           'id': 1,
+                                                           'body':
+                                                               'Comment 1'},
+                                                          {'postId': 1,
+                                                           'id': 2,
+                                                           'body':
+                                                               'Comment 2'},
+                                                          {'postId': 2,
+                                                           'id': 3,
+                                                           'body':
+                                                               'Comment 3'}]
             # Ustaw efekt dla każdego wywołania z requests.get
-            mocked_get.side_effect = [mocked_posts_response, mocked_comments_response]
+            mocked_get.side_effect = [mocked_posts_response,
+                                      mocked_comments_response]
 
             # Wywołaj funkcję z parametrami limit_posts=2 i limit_comments=2
-            with app.test_request_context('/posts/2/2?min_chars=0&max_chars=500'):
+            with app.test_request_context(
+                    '/posts/2/2?min_chars=0&max_chars=500'):
                 response = self.app.get('/posts/2/2')
 
                 # Sprawdź, czy odpowiedź ma status kodu 200
