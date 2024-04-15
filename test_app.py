ae80683332af60b704b3da22fb9d2b72216a4f6c
MichalNeuman <146724475+MichalNeuman@users.noreply.github.com>
2024-04-15 20:51:17 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,16 +8,21 @@ def client():
         yield client
 
 
-def test_get_posts(client):
+def test_index_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_posts_page(client):
     response = client.get('/posts')
     assert response.status_code == 200
 
 
-def test_get_albums(client):
+def test_albums_page(client):
     response = client.get('/albums')
     assert response.status_code == 200
 
 
-def test_get_photos(client):
+def test_photos_page(client):
     response = client.get('/albums/1/photos/albumTitle')
     assert response.status_code == 200
